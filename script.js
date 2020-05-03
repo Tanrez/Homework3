426b0997924f846fdfd9d1af7c533804cba31d8b
Cristian <Tanrezz@gmail.com>
2020-05-02 21:24:52 -0500

    2nd commit

--- script.js
+++ script.js
@@ -8,38 +8,27 @@ form.addEventListener('checkbox', i => {
     const password = generatePassword(includeLowercase, includeUppercase, includeNumbers, includeSymbols)
     passwordDisplay.innerText = password
   })
-//generate random password
+//generate password
 function generate(){
- 
-    //set password length/complexity
     var complexity = document.getElementById("slider").value;
-
     //possible password values
-    var values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
-
+    var values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+"
     var password = "";
-
-    //create for loop to choose password characters
+    //create loop
     for(var i = 0; i <= complexity; i++){password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));}
     //add password to textbox/display area
     document.getElementById("display").value = password;
     //add password to previously generated passwords section
-    document.getElementById("lastNums").innerHTML += password + "<br />";  
-}
+    document.getElementById("lastNums").innerHTML += password + "<br />";  }
 
 //set default length display of 68
 document.getElementById("length").innerHTML = "Length: 68";
-
 //function to set length based on slider position
 document.getElementById("slider").oninput = function(){
     if(document.getElementById("slider").value > 0) {document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;}
-
-    else {document.getElementById("length").innerHTML = "Length: 1";}
-}
+    else {document.getElementById("length").innerHTML = "Length: 1";}}
 
 //function to copy password to clipboard
 function copyPassword(){document.getElementById("display").select();
 document.execCommand("Copy");
-alert("Password copied to clipboard!");
-
-}
+alert("Password copied to clipboard!");}
